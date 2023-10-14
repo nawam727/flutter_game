5e2e29a985d33aadc73556efd6ca84866a2a6d32
THEVINDU RANSARA RATHNAYAKA <97075043+trsrathnayaka@users.noreply.github.com>
2023-10-14 19:41:49 +0530

    Home page

---
+++ flu_game/lib/Thevindu/home_screen.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_food_order/constants.dart';
+
+import '../../model.dart/product_model.dart';
+import 'components/banner_section.dart';
+import 'components/body.dart';
+import 'components/email_banner.dart';
+import 'components/footer.dart';
+import 'components/header.dart';
+import 'components/header_container.dart';
+import 'components/menu.dart';
+import 'components/product.dart';
+import 'components/services_card.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(child: ListView(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+DrawerHeader(child: Center(
+  child:   Text(
+  
+     "Dine Delish",
+  
+            style: TextStyle(
+  
+                fontSize: 25.0,
+  
+                fontWeight: FontWeight.w900,
+  
+                color: kSecondaryColor),
+  
+  ),
+),),
+
+
+
+        MobMenu(),
+      ],)),
+      body: SingleChildScrollView(
+        child: Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            //header
+            HeaderContainer(),
+            //body
+            BodyContainer(),
+            //footer
+            SizedBox(height: 30,),
+            Footer(),
+            //now we make our website responsive
+          ],
+        ),
+      ),
+    );
+  }
+}
+
